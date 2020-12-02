ea0ae64411d9ec3650c7fa1c433e20d0319277bb
pavlenko-aa <72490554+pavlenko-aa@users.noreply.github.com>
2020-12-02 21:47:20 +0300

    Delete example.sql

--- example.sql
+++
@@ -1,8 +0,0 @@
-drop database if exists example;
-create database example;
-use example;
-create table users(
-	id INT UNSIGNED NOT NULL PRIMARY KEY,
-  name VARCHAR(255) COMMENT 'Имя'
-);
-describe users;
